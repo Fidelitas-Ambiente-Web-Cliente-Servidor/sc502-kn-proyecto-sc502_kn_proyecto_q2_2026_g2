937fe1251f98f88722cc6e0e4665618805d2ae9b
AndrosR16 <123216631+AndrosR16@users.noreply.github.com>
2026-06-29 03:51:08 -0600

    Registro.
    Registro y Validaciones.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const formularioLogin = document.getElementById("formLogin");
+    const formularioRegistro = document.getElementById("formRegistro");
 
     if (formularioLogin) {
         formularioLogin.addEventListener("submit", iniciarSesion);
+    }
+
+    if (formularioRegistro) {
+        formularioRegistro.addEventListener("submit", registrarUsuario);
     }
 
     const correoGuardado = localStorage.getItem("correoRecordado");
@@ -39,10 +44,73 @@ function iniciarSesion(evento) {
     mensaje.textContent = "Inicio de sesión correcto.";
     mensaje.style.color = "green";
 
+    console.log("Inicio de sesión correcto");
     console.log("Correo ingresado:", correo);
-    console.log("Recordar sesión:", recordarSesion);
 
     setTimeout(function () {
         window.location.href = "index.html";
+    }, 1000);
+}
+
+function registrarUsuario(evento) {
+    evento.preventDefault();
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const correo = document.getElementById("correo").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const tipoUsuario = document.getElementById("tipoUsuario").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const mensaje = document.getElementById("mensajeRegistro");
+
+    if (
+        nombre === "" ||
+        correo === "" ||
+        telefono === "" ||
+        tipoUsuario === "" ||
+        password === "" ||
+        confirmPassword === ""
+    ) {
+        mensaje.textContent = "Debe completar todos los campos.";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    if (!/^[0-9]+$/.test(telefono)) {
+        mensaje.textContent = "El teléfono solo puede contener números.";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    if (telefono.length !== 8) {
+        mensaje.textContent = "El teléfono debe tener 8 dígitos.";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        mensaje.textContent = "Las contraseñas no coinciden.";
+        mensaje.style.color = "red";
+        return;
+    }
+
+    const usuario = {
+        nombre: nombre,
+        correo: correo,
+        telefono: telefono,
+        tipoUsuario: tipoUsuario,
+        password: password
+    };
+
+    localStorage.setItem("usuarioRegistrado", JSON.stringify(usuario));
+
+    mensaje.textContent = "Usuario registrado correctamente.";
+    mensaje.style.color = "green";
+
+    console.log("Usuario registrado correctamente");
+    console.log(usuario);
+
+    setTimeout(function () {
+        window.location.href = "login.html";
     }, 1000);
 }
